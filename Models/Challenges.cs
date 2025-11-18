a26f78ecce7df09d6e07250623bda1d80c83e545
Yuvin <yuvinpadayachee@gmail.com>
2025-11-18 12:51:52 +0200

    Tried to make a model for the challenges but messed up

---
+++ Models/Challenges.cs
@@ -0,0 +1,21 @@
+﻿namespace API.Models
+{
+    public class Challenges
+    {
+        public RiotChallenges.TotalPoints totalPoints { get; set; }
+        public RiotChallenges.CategoryPoints categoryPoints { get; set; }
+        public ChallengeList challengeList { get; set; }
+    }
+
+    public class ChallengeList
+    {
+        public string? id { get; set; }
+        public string? name  { get; set; }
+        public string? description   { get; set; }
+        public string? level { get; set; }
+        public string? value { get; set; }
+        public string? percentile    { get; set; }
+        public string? position  { get; set; }
+        public string? playersInLevel    { get; set; }
+    }
+}
